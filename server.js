ecfc3975d47a1c6a7aeb7a5cc44f2bd23c8d311f
MN Paul <87216532+MNPaul@users.noreply.github.com>
2022-10-08 13:05:41 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("API is working.")
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>console.log("app is running at port",PORT)
+)
